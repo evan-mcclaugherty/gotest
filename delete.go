2bef76462e601a32a5c912952ebc5fedab5961cd
McClaugherty <mcclau@cobank2.corp>
2019-05-30 10:33:21 -0600

    replace with http server

--- delete.go
+++ delete.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func Welcome(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "<h1>Welcome to the Go toolset.</h1>")
+
+}
 
 func main() {
-	fmt.Println("hello")
+
+	fmt.Println("Hello.")
+	fmt.Println("Starting http server.")
+	// Register handler function
+	http.HandleFunc("/welcome", Welcome)
+	fmt.Println("Go to localhost:8080/welcome")
+	fmt.Println("To terminate press CTRL+C.")
+	// Start server
+	http.ListenAndServe(":8080", nil)
+
 }
